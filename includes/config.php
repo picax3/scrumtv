841c0dea93e1a2e3746e2f1f968afa18e87f9541
sav-chris <christopher.savini@gmail.com>
2025-04-05 21:26:39 +1000

    Adding docker scripts

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,8 @@ session_start();
 date_default_timezone_set("Pacific/Auckland");
 
 try {
-    $con = new PDO("mysql:dbname=scrumtv;host=127.0.0.1", "root", "");
+    //$con = new PDO("mysql:dbname=scrumtv;host=127.0.0.1", "root", "");
+    $con = new PDO("mysql:dbname=scrumtv;host=db", "root", "example");
     $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e) {
